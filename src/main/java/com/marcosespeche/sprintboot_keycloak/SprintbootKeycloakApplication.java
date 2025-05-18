f7a7bba955def23d5ec6e3f46d26915b2613d925
marcosespeche <marcos.espeche.28@gmail.com>
2025-05-18 00:36:58 -0300

    Proyecto base

---
+++ src/main/java/com/marcosespeche/sprintboot_keycloak/SprintbootKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.marcosespeche.sprintboot_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosespeche/sprintboot_keycloak/SprintbootKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosespeche.sprintboot_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
